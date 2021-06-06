8914657dd032be023c1b0956532f19d405b3bde7
Yeshwanth <yeshwanthreddy32@gmail.com>
2021-06-06 11:07:40 -0400

    add transparent nav bar with light and dark mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,10 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import PropTypes from 'prop-types';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,18 +22,50 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
+// export default function ButtonAppBar(light) {
+//   console.log(light);
+//   const classes = useStyles();
 
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography edge="end" variant="h6" className={classes.title}>
-            Test
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+//   return (
+//     <div>
+//       <AppBar
+//         position="static"
+//         style={{ background: 'transparent', boxShadow: 'none' }}
+//       >
+//         <Toolbar variant="regular">
+//           <Typography variant="h6" className={classes.title}>
+//             Test
+//           </Typography>
+//           <IconButton onClick={light}>
+//             <Brightness4Icon />
+//           </IconButton>
+//         </Toolbar>
+//       </AppBar>
+//     </div>
+//   );
+// }
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <AppBar
+          position="static"
+          style={{ background: 'transparent', boxShadow: 'none' }}
+        >
+          <Toolbar variant="regular">
+            <Typography style={{ flexGrow: 1 }}>Test</Typography>
+            <IconButton onClick={this.props.changeLight}>
+              <Brightness4Icon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
+
+export default NavBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import * as React from 'react';
-
+import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import NavBar from '../components/NavBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import ReactDOM from 'react-dom';
 
 const themeLight = createMuiTheme({
   palette: {
@@ -27,26 +27,30 @@ const themeDark = createMuiTheme({
   },
 });
 
-// markup
-const IndexPage = () => {
-  const [light, setLight] = React.useState(true);
-  // if (
-  //   window.matchMedia &&
-  //   window.matchMedia('(prefers-color-scheme: dark)').matches
-  // ) {
-  //   light = false;
-  //   console.log('dark mode is enabled');
-  // }
-  return (
-    <main>
-      <MuiThemeProvider theme={light ? themeLight : themeDark}>
-        <CssBaseline />
-        <NavBar />
-        <Button onClick={() => setLight((prev) => !prev)}>Toggle Theme</Button>
-        <p>Work in progress</p>
-      </MuiThemeProvider>
-    </main>
-  );
-};
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      light: false,
+    };
+    //this.changeLight = this.changeLight.bind(this);
+  }
+  changeLight = () => {
+    this.setState({ light: !this.state.light });
+  };
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider theme={this.state.light ? themeLight : themeDark}>
+          <CssBaseline />
+          <NavBar
+            changeLight={this.changeLight}
+            theme={this.state.light ? themeLight : themeDark}
+          />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
 
 export default IndexPage;
